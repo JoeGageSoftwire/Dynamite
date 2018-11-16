27dc1ffe2bb06886462300aa6eca43e054400aef
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-16 17:21:56 +0000

    Add great bot

--- botCotton.js
+++ botCotton.js
@@ -1,6 +1,7 @@
 class Bot {
     constructor(){
-        this.dynamite = 100;
+        this.myDynamite = 100
+        this.opponentDynamite = 100;
         this.p1Score = 0;
         this.p2Score = 0;
         this.roundValue = 1;
@@ -19,9 +20,25 @@ class Bot {
                 ++this.roundValue;
         }
 
-        if (this.roundValue > 3 && this.dynamite > 0) {
-            --this.dynamite;
-            return 'D';
+        const opponentLastMove = this.getOpponentLastMove(gamestate);
+        if (opponentLastMove == 'D') {
+            --this.opponentDynamite
+        }
+
+        if (this.roundValue > 3 && this.myDynamite > 0) {
+            const doIDynamite = Math.random();
+            const doIWater = this.opponentDynamite > 0 ? Math.random() : 0;
+
+            if (doIDynamite < 0.25) {
+                if (doIWater < 0.1) {
+                    return this.randomRps();
+                }
+                return 'W';
+            }
+            else {
+                --this.myDynamite;
+                return 'D';
+            }
         }
         else {
             return this.randomRps();
@@ -35,6 +52,15 @@ class Bot {
             case 1: return 'P';
             case 2: return 'S';
             default: return 'R';
+        }
+    }
+
+    getOpponentLastMove(gamestate) {
+        if (gamestate.rounds.length == 0) {
+            return;
+        }
+        else {
+            return gamestate.rounds.slice(-1)[0].p2;
         }
     }
 
